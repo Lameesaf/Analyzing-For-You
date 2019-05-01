8c6574a214479043d1bc0acb6ea51ec3543f6fcb
Lamees AlFallaj <lafallaj2@gmail.com>
2019-05-02 01:12:01 +0300

    everything is complete

--- main.js
+++ main.js
@@ -1,31 +1,51 @@
-$(document).ready(function() { 
+$(document).ready(function() {
+    $('#stahp').hide()
+    $('#convert-button').hide();
+    $('.restart').hide()
+    $('.speech').hide()
+    $('.analyze-container').hide()
+
+
 
     let recorder = RecordRTC(MediaStream || HTMLElement, {});
-    var text = ' several tornadoes touch down as a line of severe thunderstorms swept through Colorado on Sunday  ';
+    var text = ' fuck off  ';
 
     function speechToText() {
         $.ajax({
 
             url: "https://gateway-wdc.watsonplatform.net/speech-to-text/api/v1/recognize",
             method: "POST",
-            data: ($('#choose-button').prop('files')[0] || recorder.getBlob()),
+            data: $('#choose-button').prop('files')[0],
             processData: false,
             headers: {
                 "Content-Type": "audio/webm",
             },
             success: function(result) {
                 console.log(result.results[0].alternatives[0].transcript)
-                text = result.results[0].alternatives[0].transcript;
-                // text = 'fuck off'
-                toneAnalyzer();
+                $('.speech').show()
+                $('.restart').show()
+                $('#converted-speech').val(result.results[0].alternatives[0].transcript)
+                $('#convert-button').hide();
+                $('.audio').hide();
+                $('#stahp').hide()
             },
-            error: console.log,
+            error: function() {
+                console.log,
+                    $('.speech').show()
+                $('#converted-speech').text('There some error in the file')
+            },
             beforeSend: function(xhr) {
                 xhr.setRequestHeader("Authorization", "Basic " + btoa('apikey:T5S_NZTysSFt8OYEkVGKAe0mnZ0ru0D2bEx8T1AJ8god'))
             }
 
         })
     }
+    $('#analyze-button').on('click', function() {
+        $('.analyze').empty();
+        console.log($('#converted-speech').val());
+        text = $('#converted-speech').val()
+        toneAnalyzer()
+    })
 
     function toneAnalyzer() {
         var url = "https://gateway-wdc.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21&text=" + text
@@ -34,21 +54,37 @@ $(document).ready(function() {
             url: url,
             method: "GET",
             success: function(result) {
-                console.log(result.document_tone.tones[0].tone_name)
-                    // console.log(result)
+                console.log(result.document_tone)
+                $('.analyze-container').show()
+                result.document_tone.tones.forEach(emotion => {
+                    $('.analyze').append($('<p/>').attr('id', 'analyze').text(emotion.tone_name))
+                });
+
             },
-            error: console.log,
+            error: function() {
+                console.log
+                $('.analyze-container').show()
+                $('#analyze').text("there nothing clear to detrmine the feelings")
+
+            },
             beforeSend: function(xhr) {
                 xhr.setRequestHeader("Authorization", "Basic " + btoa('apikey:UppglsL0bxX_hVoqW9wuOYXAr6ZIj54yLpqMZKBidA0s'))
             }
         })
     }
 
-    $('#newfile').on('click', function() {
+    $('#choose-button').on('click', function() {
+        $('#convert-button').show();
+        $('#toneanalyzer').hide()
 
 
+    })
+
+    $('#start').on('click', function() {
+        $('#stahp').show()
+        $('#toneanalyzer').hide()
         navigator.mediaDevices.getUserMedia({
-          audio: {echoCancellation:true},
+            audio: { echoCancellation: true },
             video: false,
             audio: true
         }).then(async function(stream) {
@@ -57,28 +93,29 @@ $(document).ready(function() {
             });
             recorder.startRecording();
 
-            // const sleep = m => new Promise(r => setTimeout(r, m));
-            // await sleep(10000);
             $('#stahp').on('click', function() {
-                // sleep(0)
                 recorder.stopRecording(function() {
                     let blob = recorder.getBlob();
                     invokeSaveAsDialog(blob);
 
                 });
-                console.log(recorder.getBlob())
-                    // speechToText();
+
             })
 
 
         });
     })
-
-
+    $('#newfile').on('click', function() {
+        $('.audio').show();
+        $('#toneanalyzer').show()
+    })
+    $('#toneanalyzer').on('click', function() {
+        $('.speech').show()
+    })
 
     $('#convert-button').on('click', function() {
-        // speechToText($('<input>').files[0])
         speechToText()
+
     })
 
 
